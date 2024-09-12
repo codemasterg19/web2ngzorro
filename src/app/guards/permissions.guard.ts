240564e35c65db9ee669d716c157026170535806
codemasterg19 <pablo_jmz1@hotmail.com>
2024-09-12 18:32:56 -0500

    mejorado

--- src/app/guards/permissions.guard.ts
+++ src/app/guards/permissions.guard.ts
@@ -1,13 +1,20 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { UsersService } from '../services/users/users.service';
 import { inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const permissionsGuard: CanActivateFn = (route, state) => {
 
   const usersService = inject (UsersService);
   const router = inject(Router);
+  const snackBar = inject(MatSnackBar); // Inyecta MatSnackBar
 
   if (usersService.getCurrentUser() === null) {
+    snackBar.open('Debe iniciar sesión para acceder a esta página.', 'Cerrar', {
+      duration: 3000, // Duración del mensaje (en milisegundos)
+      verticalPosition: 'top', // Posición del mensaje en la parte superior
+      horizontalPosition: 'center', // Posición centrada horizontalmente
+    });
     router.navigate(['/login']);
     return false;
   };
